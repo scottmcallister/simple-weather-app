66ea507a001456fb1f560f6b4f2af9de88e39342
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-01-18 22:59:41 -0500

    improve styling a bit

--- app/components/weatherSearch.js
+++ app/components/weatherSearch.js
@@ -8,11 +8,16 @@ import styles from '../styles';
 
 const WeatherSearch = () =>
     (<View style={styles.container}>
-      <Text style={styles.message}>Search</Text>
-      <TextInput
-        style={styles.searchbox}
-        value={''}
-      />
+      <View style={styles.textContainer}>
+        <Text style={styles.message}>Search</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchbox}
+          value={''}
+        />
+      </View>
+      <View style={styles.paddingBelow} />
     </View>);
 
 export default WeatherSearch;
--- app/styles.js
+++ app/styles.js
@@ -1,24 +1,45 @@
 import { StyleSheet } from 'react-native';
 
 const gray = '#cccccc';
+const blue = '#5DBCD2';
+const white = '#ffffff';
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 3,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: blue,
     },
     message: {
         fontSize: 20,
         textAlign: 'center',
         margin: 10,
+        color: white,
     },
     searchbox: {
         textAlign: 'center',
         margin: 10,
-        flex: 1,
         height: 40,
         borderColor: gray,
         borderWidth: 1,
+        flex: 1,
+        flexDirection: 'row',
+        alignSelf: 'stretch',
+        backgroundColor: white,
+    },
+    searchContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    textContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    paddingBelow: {
+        flex: 1,
     },
 });
 
