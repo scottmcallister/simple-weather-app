97e4b68844f3e0a90c0844822ca0ca81bf017e14
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-01-27 21:22:02 -0500

    switch icons based on sunrise/sunset

--- app/components/weatherInfo.js
+++ app/components/weatherInfo.js
@@ -6,11 +6,16 @@ import _ from 'lodash';
 import styles from '../styles';
 import utils from '../utils';
 
-const genIcon = (description) => {
+const genIcon = (description, sys) => {
+    const { sunrise, sunset } = sys;
+    const rightNow = Date.now() / 1000;
+    const dayTime = rightNow > sunrise && rightNow < sunset;
     let icon;
     switch (description) {
         case 'Haze':
-            icon = (<Icon name="ios-cloudy" style={styles.weatherIcon} size={40} color="#FFFFFF" />);
+            icon = dayTime ?
+                (<Icon name="ios-cloudy" style={styles.weatherIcon} size={40} color="#FFFFFF" />) :
+                (<Icon name="ios-cloudy-night" style={styles.weatherIcon} size={40} color="#FFFFFF" />);
             break;
         case 'Snow':
             icon = (<Icon name="ios-snow" style={styles.weatherIcon} size={40} color="#FFFFFF" />);
@@ -19,7 +24,9 @@ const genIcon = (description) => {
             icon = (<Icon name="ios-cloudy" style={styles.weatherIcon} size={40} color="#FFFFFF" />);
             break;
         case 'Clear':
-            icon = (<Icon name="ios-sunny" style={styles.weatherIcon} size={40} color="#FFFFFF" />);
+            icon = dayTime ?
+                (<Icon name="ios-sunny" style={styles.weatherIcon} size={40} color="#FFFFFF" />) :
+                (<Icon name="ios-moon" style={styles.weatherIcon} size={40} color="#FFFFFF" />);
             break;
         case 'Rain':
             icon = (<Icon name="ios-rainy" style={styles.weatherIcon} size={40} color="#FFFFFF" />);
@@ -42,7 +49,7 @@ const renderContent = weatherData =>
           <View>
             <Text style={styles.weatherInfo}>{`${weatherData.name}`}</Text>
             <Text style={styles.weatherInfo}>{`${utils.toCelsius(weatherData.main.temp)} °C`}</Text>
-            {genIcon(weatherData.weather[0].main)}
+            {genIcon(weatherData.weather[0].main, weatherData.sys)}
             <Text style={styles.weatherInfo}>{`${weatherData.weather[0].description}`}</Text>
           </View>
         }
